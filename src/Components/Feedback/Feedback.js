c7a2cffee7ae68880260a5dc94384e22406d9128
sensei <anona1764@gmail.com>
2022-11-25 21:54:34 +0500

    fixed feedback

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feedback.css";
 
 export function Feedback() {
+  const [project, setProject] = useState({
+    fullname: "",
+    phone: "",
+    about: "",
+  });
+
   return (
     <div id="feedback">
       <p>You can leave your feedback here</p>
-      <form className="feedback-form">
-        <input type="text" placeholder="full name" autoComplete="off" />
-        <input type="text" placeholder="phone number" autoComplete="off" />
-        <textarea placeholder="about"></textarea>
+      <form
+        className="feedback-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(project);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="full name"
+          autoComplete="off"
+          onChange={(e) => setProject({ ...project, fullname: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="phone number"
+          autoComplete="off"
+          onChange={(e) => setProject({ ...project, phone: e.target.value })}
+        />
+        <textarea
+          placeholder="about"
+          onChange={(e) => setProject({ ...project, about: e.target.value })}
+        ></textarea>
         <button type="submit" className="my-projects-link">
           Send Feedback
         </button>
