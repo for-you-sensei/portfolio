5e3f87adcd0202c840a8e2abbc61333b3ad94bd7
sensei <anona1764@gmail.com>
2022-11-28 15:48:31 +0500

    fixed add feedbacks

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -16,22 +16,26 @@ export function Feedback() {
         onSubmit={(e) => {
           e.preventDefault();
           console.log(project);
+          setProject({ fullname: "", phone: "", about: "" });
         }}
       >
         <input
           type="text"
           placeholder="full name"
           autoComplete="off"
+          value={project.fullname}
           onChange={(e) => setProject({ ...project, fullname: e.target.value })}
         />
         <input
           type="text"
           placeholder="phone number"
           autoComplete="off"
+          value={project.phone}
           onChange={(e) => setProject({ ...project, phone: e.target.value })}
         />
         <textarea
           placeholder="about"
+          value={project.about}
           onChange={(e) => setProject({ ...project, about: e.target.value })}
         ></textarea>
         <button type="submit" className="my-projects-link">
